d7bbc585e890e982be8170ca6d65ec742982cea6
Mellert, Dennis <dennis.mellert@otto.de>
2020-05-13 22:28:22 +0200

    env variables for aws

--- .config/zsh/env/env.zsh
+++ .config/zsh/env/env.zsh
@@ -1,4 +1,7 @@
 # AWS 
-export AWS_PROFILE=ops-admin
+#export AWS_PROFILE=dev-admin
+export AWS_PROFILE=dev
 export AWS_DEFAULT_REGION=eu-central-1
+export AWS_SDK_LOAD_CONFIG=1
 
+export PATH=$PATH:~/.local/bin
